76d21cc8a04ce725e195a49b479cb7f0db4280dd
kingsframe <kaizhangwen@gmail.com>
2022-11-12 05:20:22 -0800

    add test yml

--- programs/starter-project/src/lib.rs
+++ programs/starter-project/src/lib.rs
@@ -5,7 +5,7 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod starter_project {
     use super::*;
-    // Test clippy github actions with this line of comment
+
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
